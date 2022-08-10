4eb086548e4175069b392a659a4b3196409053de
aalperenbahar <aalperenbahar@gmail.com>
2022-08-10 12:21:54 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+resource "docker_container" "helloworld" {
+  image = "helloworld:latest"
+  name  = "helloworld"
+  restart = "always"
+  volumes {
+    container_path  = "/var/lib/docker"
+    # replace the host_path with full path for your project directory starting from root directory /
+    host_path = "/Users/alperenbahar/Desktop/Projects/py-dockerex" 
+    read_only = false
+  }
+  ports {
+    internal = 5000
+    external = 8080
+  }
+}
