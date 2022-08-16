e87f3eb9114405dcdb483a83b70045cbbc188844
Ahmet Alperen Bahar <45522794+aalperenbahar@users.noreply.github.com>
2022-08-16 14:58:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   #required_providers {
-   # docker = {
-    #  source  = "kreuzwerker/docker"
-     # version = "~> 2.13.0"
-   # }
+  # docker = {
+  #  source  = "kreuzwerker/docker"
+  # version = "~> 2.13.0"
+  # }
   #}
   cloud {
     organization = "aaalperenbahar"
@@ -18,13 +18,13 @@ provider "docker" {}
 
 
 resource "docker_container" "helloworld" {
-  image = "helloworld:latest"
-  name  = "helloworld"
+  image   = "helloworld:latest"
+  name    = "helloworld"
   restart = "always"
   volumes {
-    container_path  = "/var/lib/docker"
+    container_path = "/var/lib/docker"
     # replace the host_path with full path for your project directory starting from root directory /
-    host_path = "/Users/alperenbahar/Desktop/Projects/py-dockerex" 
+    host_path = "/Users/alperenbahar/Desktop/Projects/py-dockerex"
     read_only = false
   }
   ports {
